4a2a105a45f5e11737abe8808bed7b15e99b1646
aldoprogrammer <aldobesma@gmail.com>
2024-02-27 19:52:46 +0700

    update

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,14 +1,15 @@
 import { Button } from '@material-tailwind/react';
 import axios from '../config/config';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 const Dashboard = () => {
     const navigate = useNavigate();
+    const [reloadKey, setReloadKey] = useState(0); // State for reloading data
 
     useEffect(() => {
-        axios.get('/auth/verify')
+        axios.get(`/auth/verify?reloadKey=${reloadKey}`)
         .then(res => {
             if (!res.data.status) {
                 navigate('/login');
@@ -18,11 +19,11 @@ const Dashboard = () => {
             console.error('Error verifying user:', err);
             navigate('/login');
         });
-    }, [navigate]);
+    }, [navigate, reloadKey]);
 
     const handleLogout = async () => {
         try {
-            const response = await axios.get('/auth/logout');
+            const response = await axios.get(`/auth/logout?reloadKey=${reloadKey}`);
             if (response.data.status) {
                 toast.success('Logout Successfully');
                 navigate('/login');
